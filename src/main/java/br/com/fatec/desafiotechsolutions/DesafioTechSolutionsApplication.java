c7f8737ffe6ebfd9d9e336da29c8e20c21a6540f
dti <adriano.rodrigues365@outlook.com>
2026-09-04 10:49:57 -0300

    atividade 1 desafito Techsolutions

---
+++ src/main/java/br/com/fatec/desafiotechsolutions/DesafioTechSolutionsApplication.java
@@ -0,0 +1,14 @@
+package br.com.fatec.desafiotechsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTechSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioTechSolutionsApplication.class, args);
+    }
+
+}
+
